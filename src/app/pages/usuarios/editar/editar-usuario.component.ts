87d59dae6112afef30baee70a787c515d7ae6fa1
Moreno Lucas Omar <ing.lucasmoreno@gmail.com>
2023-11-05 14:17:44 -0300

    Bug fixed

--- src/app/pages/usuarios/editar/editar-usuario.component.ts
+++ src/app/pages/usuarios/editar/editar-usuario.component.ts
@@ -122,10 +122,12 @@ export class EditarUsuarioComponent implements OnInit {
     // Se agregan los permisos
     let data: any = this.usuarioForm.value;
 
-    if(role !== 'ADMIN_ROLE') data.permisos = this.adicionarPermisos(); // Se adicionan los permisos a la data para actualizacion
-    else data.permisos = [];
+    // if(role !== 'ADMIN_ROLE') data.permisos = this.adicionarPermisos(); // Se adicionan los permisos a la data para actualizacion
+    // else data.permisos = [];
 
     this.alertService.loading();
+
+    console.log(data);
 
     this.usuariosService.actualizarUsuario(this.id, data).subscribe(() => {
       this.alertService.close();
--- src/app/shared/header/items-ventas.ts
+++ src/app/shared/header/items-ventas.ts
@@ -2,8 +2,8 @@
 // ITEMS PARA BARRA DE NAVEGACION - VENTAS
 
 export const itemsVentas = [
-  { svg: '', route: 'presupuestos', item: 'Presupuestos', permiso:['PRESUPUESTOS_NAV'] },
-  { svg: '', route: 'ventas', item: 'Ventas directas', permiso:['VENTAS_DIRECTAS_NAV'] },
+  // { svg: '', route: 'presupuestos', item: 'Presupuestos', permiso:['PRESUPUESTOS_NAV'] },
+  // { svg: '', route: 'ventas', item: 'Ventas directas', permiso:['VENTAS_DIRECTAS_NAV'] },
   { svg: '', route: 'ventas-propias', item: 'Ventas propias', permiso:['VENTAS_PROPIAS_NAV'] },
   { svg: '', route: 'cobros', item: 'Recibos de cobro', permiso:['RECIBOS_COBRO_NAV'] },
   { svg: '', route: 'clientes', item: 'Clientes', permiso:['CLIENTES_NAV'] },
